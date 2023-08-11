efe0388cd52c5b810585ec5d17a2b757c0640c89
yasserweissa <yasserws998@gmail.com>
2023-08-11 00:51:01 -0700

    101 fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,12 @@
 /**
  * main - Entry Point
  * Return: 1
- * Description: Print without printf or puts, and display error
+ * Description: Print without printf or puts, and return error
  */
 
-int main (void)
+int main(void)
 {
-	write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
+	strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 	return (1);
 }
