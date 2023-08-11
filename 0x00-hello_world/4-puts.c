8ce44656db4547c8906f2e48747ea769e4f35ff1
yasserweissa <yasserws998@gmail.com>
2023-08-10 19:43:14 -0700

    4 fix for betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/*
+ * main
+ *
+ * returns 0
+ *
+ * print using puts
+ * /
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
