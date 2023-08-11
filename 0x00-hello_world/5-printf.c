6d6609ec1b6994cc7c46169d95c7810060890fde
yasserweissa <yasserws998@gmail.com>
2023-08-10 19:59:38 -0700

    5 fix2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
- *
+ * 
  * Return: 0
  * 
  * Description: print using printf
@@ -13,3 +13,4 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
