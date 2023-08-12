8e531e697e31da39c5d76f8dcfb28a0e3a7cd8bd
yasserweissa <yasserws998@gmail.com>
2023-08-11 19:31:54 -0700

    8 fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - 
+ * main - print hexadecimal numbers using putchar
  * Return: 0
  */
 
 int main(void)
 {
-	for (int n = 48; n < 58; n++)
+	int n;
+
+	for (n = 48; n < 58; n++)
 		putchar(n);
-	for (int n = 97; n < 103; n++)
+	for (n = 97; n < 103; n++)
 		putchar(n);
-	printf(10);
-	return 0;
+	putchar(10);
+	return (0);
 }
