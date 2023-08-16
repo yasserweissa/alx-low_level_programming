938319658c6e7ed68bab06fb20c4cf81fabccfd1
yasserweissa <yasserws998@gmail.com>
2023-08-15 18:44:10 -0700

    0\ try again

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
- * _putchar - prints a character
- * @c: character to print
- * Return: 1 if successful, 0 if there was an error
+ * main - prints _putchar using _putchar
+ * Return: 0 
 */
-int _putchar(char c)
+
+int main(void)
 {
-	return (write(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
