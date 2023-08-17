d9f60e56229102983c83bf7c017bd9a8aeec036f
yasserweissa <yasserws998@gmail.com>
2023-08-16 18:34:34 -0700

    100-times_table bettyfix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,9 +18,8 @@ void print_times_table(int n)
 			{
 				k = j * i;
 				if (j == 0)
-				{
 					_putchar(k + '0');
-				}
+
 				else if (k < 10 && j != 0)
 				{
 					_putchar(',');
@@ -45,8 +44,7 @@ void print_times_table(int n)
 					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
 				}
-			}
-			_putchar('\n');
+			} _putchar('\n');
 		}
 	}
 }
