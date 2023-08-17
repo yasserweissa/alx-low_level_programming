9a0d65f6b888e0b0fd8e9235cbd7f495e2e5ea95
yasserweissa <yasserws998@gmail.com>
2023-08-16 18:26:54 -0700

    100-times_table fix3

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,11 +6,11 @@
  * Return: no return
  */
 
-void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n < 16 && n > 0)
+
+	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -19,34 +19,34 @@ void print_times_table(int n)
 				k = j * i;
 				if (j == 0)
 				{
-					_putchar(k + '0');
+					putchar(k + '0');
 				}
 				else if (k < 10 && j != 0)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} 
+					putchar(',');
+					putchar(' ');
+					putchar(' ');
+					putchar(' ');
+					putchar(k + '0');
+				}
 				else if (k >= 10 && k < 100)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
+					putchar(',');
+					putchar(' ');
+					putchar(' ');
+					putchar((k / 10) + '0');
+					putchar((k % 10) + '0');
 				}
 				else if (k >= 100)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
+					putchar(',');
+					putchar(' ');
+					putchar((k / 100) + '0');
+					putchar(((k / 10) % 10) + '0');
+					putchar((k % 10) + '0');
 				}
-				_putchar('\n');
 			}
+			putchar('\n');
 		}
 	}
 }
