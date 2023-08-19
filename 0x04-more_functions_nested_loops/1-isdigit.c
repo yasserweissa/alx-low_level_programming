8291e8a33a68fa857979abb715aec18bf6503cab
yasserweissa <yasserws998@gmail.com>
2023-08-18 17:13:48 -0700

    0x04 0-3

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if a character is a digit
+ * @c: given character to be checked
+ * Return: 1 if digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - multiplies two numbers
+ * @a: a - 1st number
+ * @b: b - 2nd number
+ * Return: product of multiplication
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
+
