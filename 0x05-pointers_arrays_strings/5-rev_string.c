a2e4b980f6fd8f5a4bd838d8734f02d3d1aa23e9
yasserweissa <yasserws998@gmail.com>
2023-08-21 02:51:10 -0700

    0x05 6fixgit add .

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,7 +12,7 @@ void rev_string(char *s)
 	for (length = 0; s[length] != '\0';)
 		length++;
 
-	for (i = 0; i <= length / 2; i++)
+	for (i = 0; i < length / 2; i++)
 	{
 		temp = s[i];
 		s[i] = s[length - 1 - i];
