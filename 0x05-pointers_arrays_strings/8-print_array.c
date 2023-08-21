13e305ff79ef47e7e23358c8cd04515bedee03fc
yasserweissa <yasserws998@gmail.com>
2023-08-21 01:48:03 -0700

    0x05 8fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,9 +8,12 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
+	if (n > 0)
+	{
+		int i;
 
-	for (i = 0; i < (n - 1); i++)
-		printf("%d, ", a[i]);
-	printf("%d\n", a[n - 1]);
+		for (i = 0; i < (n - 1); i++)
+			printf("%d, ", a[i]);
+		printf("%d\n", a[n - 1]);
+	}
 }
