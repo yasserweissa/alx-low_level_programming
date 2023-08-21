213f12c2a0d517d4fcb55be4f8ada18c26ad4d19
yasserweissa <yasserws998@gmail.com>
2023-08-21 01:55:02 -0700

    0x05 7fix2

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,7 +9,7 @@ void puts_half(char *str)
 {
 	int length, i;
 
-	for (length = 0; str[length] != '\0')
+	for (length = 0; str[length] != '\0';)
 		length++;
 	for (i = 0; i < length / 2; i++)
 		_putchar(str[i]);
