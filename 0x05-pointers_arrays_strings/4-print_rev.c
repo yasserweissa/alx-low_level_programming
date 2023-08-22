1e18f5452396a63bcb6a98996956ed26f1c4b64d
yasserweissa <yasserws998@gmail.com>
2023-08-21 18:03:29 -0700

    0x05 4fix2

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,11 @@ void print_rev(char *s)
 {
 	int length, i;
 
-	for (length = 0; *s != '\0'; length++)
-		s++;
+	for (length = 0; s[length] != '\0';)
+		length++;
 
-	for (i = length - 1; i > 0; i--, s--)
-		_putchar(*s);
+	for (i = length - 1; i >= 0; i--)
+		_putchar(s[1]);
 
 	_putchar('\n');
 }
