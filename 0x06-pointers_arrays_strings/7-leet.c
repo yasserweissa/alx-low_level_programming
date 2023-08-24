30f9e7ed5a26c6d1d212e7494f52f858e1d24144
yasserweissa <yasserws998@gmail.com>
2023-08-24 01:49:42 -0700

    0x06 7fix1

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,14 +8,16 @@
 char *leet(char *s)
 {
 	int i, j;
-	char *a, *b;
-
-	a[] = "aAeEoOtTlL";
-	b[] = "4433007711";
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
 
 	for (i = 0; s[i] != '\0'; i++)
 		for (j = 0; j < 10; j++)
 			if (s[i] == a[j])
-				n[i] = b[i];
-	return (s);
+			{
+				s[i] = b[j];
+				break;
+			}
+	return s;
 }
+
