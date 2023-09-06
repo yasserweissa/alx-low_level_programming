2ce4d936e8b42d4b56288f3332bc698bf9e6195b
yasserweissa <yasserws998@gmail.com>
2023-09-05 19:11:23 -0700

    0x0B 2fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,12 +22,12 @@ char *str_concat(char *s1, char *s2)
 		i++;
 	while (s2[j] != '\0')
 		j++;
-	arr = malloc(sizeoff(char) * (i + j + 1);
+	arr = malloc(sizeof(char) * (i + j + 1));
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
 		arr[i] = s1[i];
-	for (j = 0; s2[i] != '\0'; i++, j++)
+	for (j = 0; s2[j] != '\0'; i++, j++)
 		arr[i] = s2[j];
 	arr[i] = '\0';
 	return (arr);
