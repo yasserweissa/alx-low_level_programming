7e9fe5e0c328658728e92eb46cd4bfd92a8e1d0c
yasserweissa <yasserws998@gmail.com>
2023-09-07 19:39:48 -0700

    0x0C 3fix

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,7 +12,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	size = max - min + 1;
+	range = max - min + 1;
 	p = malloc(sizeof(int) * range);
 	if (!p)
 		return (NULL);
