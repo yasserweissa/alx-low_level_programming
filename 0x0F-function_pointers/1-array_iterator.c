867dcbd48aeeecbde9ca5c8577e44baafefdd143
yasserweissa <yasserws998@gmail.com>
2023-09-13 01:11:52 -0700

    0x0F mandatory

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "functions_pointers.h"
+
+/**
+ * array_iterator - executes functions in an array
+ * @array: array of pointers to functions
+ * @size:int, size of the array
+ * @action: pointer to current function
+ * Return: no return
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+
+/**
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: Always 0.
+ **/
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	int a, b;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op = argv[2];
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*op == '/' && b == 0) || (*op == '%' && b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(a, b));
+	return (0);
+}
