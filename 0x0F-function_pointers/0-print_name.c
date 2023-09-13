b204644336958b8d1a5ace624dfc9bb9698e346a
yasserweissa <yasserws998@gmail.com>
2023-09-12 23:18:11 -0700

    0x0F 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "functions_pointers.h"
+
+/**
+ * print_name - prints a (provided) name
+ * @name: string, provided name
+ * @f:pointer to a function
+ * Return: no return
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
